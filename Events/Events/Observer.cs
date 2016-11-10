6606b85e3573831d3821cf0b0313206af1ea8dc8
a15995 <123@123.dk>
2016-11-10 11:46:50 +0100

    Events

---
+++ Events/Events/Observer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Events
+{
+    class Observer
+    {
+        public void HandleEvent(object sender, EventArgs args)
+        {
+            Debug.WriteLine("Something happened to " + sender);
+        }
+    }
+}
---
+++ Events/Events/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Observable observable = new Observable();
+            Observer observer = new Observer();
+            observable.SomethingHappened += observer.HandleEvent;
+
+            observable.DoSomething();
+        }
+    }
+}
